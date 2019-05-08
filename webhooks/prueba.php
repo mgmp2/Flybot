49d6b8913e05a5e9da41634f331464fa6ee3f7a3
Miriam Mendoza <miriam.mendoza@interseguro.com.pe>
2019-05-07 19:11:14 -0500

    Separando archivo de mail para el envio respectivo

--- webhooks/prueba.php
+++ webhooks/prueba.php
@@ -6,62 +6,55 @@
   $dataBot = new GuzzleHttp\Client(['base_uri' => 'https://api-flybot.herokuapp.com/api/']);
   $response = $client->request('GET', 'destinos/');
 
-  echo"Hola";
-  require "../PHPMailer/Exception.php";
-  require "../PHPMailer/PHPMailer.php";
-  require "../PHPMailer/SMTP.php";
+  // require "../PHPMailer/Exception.php";
+  // require "../PHPMailer/PHPMailer.php";
+  // require "../PHPMailer/SMTP.php";
+  // //
+  // use PHPMailer\PHPMailer\PHPMailer;
+  // use PHPMailer\PHPMailer\Exception;
   //
-  use PHPMailer\PHPMailer\PHPMailer;
-  use PHPMailer\PHPMailer\Exception;
-
-  $nombreX = "Gaby";
-
-
-  $oMail = new PHPMailer();
-  $oMail->isSMTP();
-  $oMail->Host="smtp.gmail.com";
-  $oMail->Port=587;
-  $oMail->SMTPSecure="tls";//protocolo de encriptacion
-  $oMail->SMTPAuth=true;
-  $oMail->Username="miriammp1997@gmail.com";
-  $oMail->Password="miriamgoogle97";
-  $oMail->setFrom('miriammp1997@gmail.com', 'Viaje Seguro'); // Nombre usuario
-  $oMail->addAddress("miriammp1997@gmail.com", '');
-
-
-
-    $username = "Miriam";
-    $destino = "Europa";
-    $canAdul = 1;
-    $canNinos = 0;
-    $fechaPartida = "24/02/2018";
-    $fechaRetorno = "24/03/2018";
-    $precio ="25.00";
-    $nroPoliza = "123";
-
-    $cabecera = "¡Ahora eres parte de Viaje Seguro! | $nroPoliza";
-    $oMail->Subject=$cabecera;
-
-
-    // Settin variables
-    $message = file_get_contents(dirname(__FILE__).'/../mail/confirmation.html');
-    $message = str_replace('%testusername%', $username, $message);
-    $message = str_replace('%destino%', $destino, $message);
-    $message = str_replace('%canAdul%', $canAdul, $message);
-    $message = str_replace('%fechaPartida%', $fechaPartida, $message);
-    $message = str_replace('%fechaRetorno%', $fechaRetorno, $message);
-    $message = str_replace('%canNinos%', $canNinos, $message);
-    $message = str_replace('%precio%', $precio, $message);
-    // $message = str_replace('%testrandomkey%', $randomkey, $message);
-    $oMail->msgHTML($message);
-
-
-
-  if (!$oMail->send()) {
-    echo"TodoBien | ";
-
-    echo $oMail->ErrorInfo;
-  } else{
-    echo$oMail;
-  }
+  //
+  //
+  // $oMail = new PHPMailer();
+  // $oMail->isSMTP();
+  // $oMail->Host="smtp.gmail.com";
+  // $oMail->Port=587;
+  // $oMail->SMTPSecure="tls";//protocolo de encriptacion
+  // $oMail->SMTPAuth=true;
+  // $oMail->Username="miriammp1997@gmail.com";
+  // $oMail->Password="miriamgoogle97";
+  // $oMail->setFrom('miriammp1997@gmail.com', 'Viaje Seguro'); // Nombre usuario
+  // $oMail->addAddress("miriammp1997@gmail.com", '');
+  //
+  //   $username = "Miriam";
+  //   $destino = "Europa";
+  //   $canAdul = 1;
+  //   $canNinos = 0;
+  //   $fechaPartida = "24/02/2018";
+  //   $fechaRetorno = "24/03/2018";
+  //   $precio ="25.00";
+  //   $nroPoliza = "123";
+  //
+  //   $cabecera = "¡Ahora eres parte de Viaje Seguro! | $nroPoliza";
+  //   $oMail->Subject=$cabecera;
+  //
+  //   // Settin variables
+  //   $message = file_get_contents(dirname(__FILE__).'/../mail/confirmation.html');
+  //   $message = str_replace('%testusername%', $username, $message);
+  //   $message = str_replace('%destino%', $destino, $message);
+  //   $message = str_replace('%canAdul%', $canAdul, $message);
+  //   $message = str_replace('%fechaPartida%', $fechaPartida, $message);
+  //   $message = str_replace('%fechaRetorno%', $fechaRetorno, $message);
+  //   $message = str_replace('%canNinos%', $canNinos, $message);
+  //   $message = str_replace('%precio%', $precio, $message);
+  //   // $message = str_replace('%testrandomkey%', $randomkey, $message);
+  //   $oMail->msgHTML($message);
+  //
+  // if (!$oMail->send()) {
+  //   echo"TodoBien | ";
+  //
+  //   echo $oMail->ErrorInfo;
+  // } else{
+  //   echo$oMail;
+  // }
  ?>
--- webhooks/segurodeviajesbot.php
+++ webhooks/segurodeviajesbot.php
@@ -2,36 +2,13 @@
   include_once "../libreria/dialogflow.php";
   require '../vendor/autoload.php';
 
-  require  "../PHPMailer/Exception.php";
-  require  "../PHPMailer/PHPMailer.php";
-  require  "../PHPMailer/SMTP.php";
-
-  use PHPMailer/PHPMailer/PHPMailer;
-  use PHPMailer/PHPMailer/Exception;
-
-  // $oMail = new PHPMailer();
-  // $oMail ->isSMTP();
-  // $oMail ->Host="smtp.gmail.com";
-  // $oMail ->Port=587;
-  // $oMail ->SMTPSECURE="tls";//protocolo de encriptacion
-  // $oMail  ->SMTPAUTH=true;
-  // $oMail->Username="miriammp1997@gmail.com";
-  // $oMail->Password="miriamgmail97";
-  // $oMail->setFrom('miriammp1997@gmail.com', 'Buen día');
-  // $oMail->addAddress("miriammp1997@gmail.com", 'Prueba2');
-  // $oMail->Subject("cabecera");
-  // $oMail->msgHTML('Cuerpo del mensaje');
-  //
-  // if (!$oMail->send()) {
-  //   echo $oMail->ErrorInfo;
-  // }
   logIn("viajesbot", "miriamgmail97");
 
   use GuzzleHttp\Client;
 
   $client = new GuzzleHttp\Client(['base_uri' => 'https://test.interseguro.pe/travel-api/api/v1/']);
   $dataBot = new GuzzleHttp\Client(['base_uri' => 'https://api-flybot.herokuapp.com/api/']);
-  // conexion a la bd FLYBOT
+  // conexion a la bd FLYBOT+
 
   include "php/pregunta_frecuente.php"
   include "php/cotizar.php"
